325e41af669a3760295352013979533717a11072
Al Anderson <al_anderson@skc.edu>
2015-06-10 21:20:35 -0600

    Wrote both required functions. Completed test successfully.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to calculate the inverse of a matrix
+## but they do it by caching calculations so they do not have to be recalculated
+## thereby increasing performance
+## 
+## Example Use:
+## c=rbind(c(1, -1/4), c(-1/4, 1))
+## c_cm <- makeCacheMatrix(c)
+## cacheSolve(c_cm)
+## -- RESULTS --
+## getting cached data
+## [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
 
-## Write a short comment describing this function
+## This function creates a special matrix that caches our inverse function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function uses our special matrix to calculate the inverse of our matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
